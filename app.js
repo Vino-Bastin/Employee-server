a5c7b05654683c031eaa66a2d6a087ff5ca3c599
Vino-Bastin <vino105264@gmail.com>
2023-03-19 18:53:03 +0530

    React Build file added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
 
 import employeeRoute from "./src/route/employeeRoute.js";
 import { expressErrorHandler } from "./src/util/errorHandler.js";
@@ -13,9 +14,17 @@ import { expressErrorHandler } from "./src/util/errorHandler.js";
 // * express app
 const app = express();
 
+// * static files
+app.use(express.static(path.resolve("build")));
+
 // * global middleware's
 app.use(bodyParser.json());
 app.use(cors());
+
+// * serve static files
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("build", "index.html"));
+});
 
 // * routes
 app.use("/api/v1/employees", employeeRoute);
